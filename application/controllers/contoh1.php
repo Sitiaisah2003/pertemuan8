c58a753e2038804e7528a83a66556ca7f0f8355e
sitiaisah2003 <sour.candy42611@gmail.com>
2022-11-13 22:58:05 +0700

    pertemuan8

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Deni Saputra
+ Saya tingga di daerah Tanjung Raya 1
+ olah raga yang saya sukai adalah
+ sepakbola";
+ }
+}
